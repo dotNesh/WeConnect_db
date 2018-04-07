abe1b5c2889c6974708a7ad18cd0dfd55cccaf77
Kariuki Kelvin <kmunene@live.com>
2018-04-07 17:39:44 +0300

    Fixed test error

--- tests/test_users.py
+++ tests/test_users.py
@@ -17,25 +17,20 @@ class UserTestcase(unittest.TestCase):
         db.init_app(app)
         with app.app_context():
             db.drop_all()
-            db.create_all()              
+            db.create_all()            
+
+        self.app().post("/api/v2/auth/register",
+                    data=json.dumps(dict(email="nina@live",username="nina",
+                                password="12345678")), content_type="application/json")      
             
     def test_create_user(self):
         response = self.app().post("/api/v2/auth/register",
-                    data=json.dumps(dict(email="nina@live",username="nina",
+                    data=json.dumps(dict(email="nesh@live",username="nesh",
                                 password="12345678")), content_type="application/json")
 
         self.assertEqual(response.status_code, 201)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertEqual(response_msg,"Account successfully registered. Log In to access.")      
-class UserMananipulationTestcase(unittest.TestCase):       
-    def setUp(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres17!@localhost/weconnect_test'
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
-        self.app = app.test_client 
-        db.init_app(app)
 
     def test_existing_username(self):
         response = self.app().post("/api/v2/auth/register",
