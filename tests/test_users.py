2e627f006d2a1ed60f3d7e7ca827f57a0ba5862c
Kariuki Kelvin <kmunene@live.com>
2018-04-07 16:39:53 +0300

    [Feature #156332639] user registration tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -26,5 +26,33 @@ class UserTestcase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 201)
         response_msg = json.loads(response.data.decode("UTF-8"))
-        self.assertEqual(response_msg,"Account successfully registered. Log In to access.")   
+        self.assertEqual(response_msg,"Account successfully registered. Log In to access.")  
+class UserMananipulationTestcase(unittest.TestCase):       
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres17!@localhost/weconnect_test'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+        self.app = app.test_client 
+        db.init_app(app)
+
+    def test_existing_username(self):
+        response = self.app().post("/api/v2/auth/register",
+                    data=json.dumps(dict(email="kmunene@live",username="nina",
+                                password="12345678")), content_type="application/json")
+
+        self.assertEqual(response.status_code, 409)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response_msg,"Username Taken!")       
+
+    def test_existing_email(self):
+        response = self.app().post("/api/v2/auth/register",
+                    data=json.dumps(dict(email="nina@live",username="kmunene",
+                                password="12345678")), content_type="application/json")
+
+        self.assertEqual(response.status_code, 409)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response_msg,"Email Already Exists!")
+         
         
