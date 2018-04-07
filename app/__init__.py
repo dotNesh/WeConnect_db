9e0af1fdfb29763ca8e6feb82e6aa054d327f861
Kariuki Kelvin <kmunene@live.com>
2018-04-07 14:38:51 +0300

    Modified init file

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+'''App module'''
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
