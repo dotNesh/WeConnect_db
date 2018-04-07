56c8b8da437654529832638fd8608a58b27853ec
Kariuki Kelvin <kmunene@live.com>
2018-04-07 16:17:10 +0300

    Modified to import from views

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ db = SQLAlchemy()
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+#To avoid circular imports
+from app import views 
