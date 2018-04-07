5a9a00212a018ba7ba309299507d74e2371917f8
Kariuki Kelvin <kmunene@live.com>
2018-04-07 16:07:37 +0300

    [Feature #156332639] user registration tests

---
+++ tests/test_users.py
@@ -0,0 +1,30 @@
+'''API User Route tests'''
+import unittest 
+import json
+from flask import Flask
+from app.models import Users, Businesses
+from app import app, db
+
+class UserTestcase(unittest.TestCase):
+    '''Test for class user'''
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres17!@localhost/weconnect_test'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+        self.app = app.test_client 
+        db.init_app(app)
+        with app.app_context():
+            db.drop_all()
+            db.create_all()              
+            
+    def test_create_user(self):
+        response = self.app().post("/api/v2/auth/register",
+                    data=json.dumps(dict(email="nina@live",username="nina",
+                                password="12345678")), content_type="application/json")
+
+        self.assertEqual(response.status_code, 201)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response_msg,"Account successfully registered. Log In to access.")   
+        
